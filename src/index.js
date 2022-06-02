58fa39cf92090b001a356dad4d2a3626a6dfb0af
whid-bb <whid11s50@gmail.com>
2022-06-02 14:08:54 +0200

    fix linter warning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import ToDoList from './js/ToDoList';
+import ToDoList from './js/ToDoList.js';
 
 const ToDo = new ToDoList({
-  listTag: 'add-task-li',
+  listTag: '#add-task-li',
+  form: {
+    tag: '#add-task-li form',
+    inputName: 'add-task',
+  },
 });
 
 ToDo.init();
